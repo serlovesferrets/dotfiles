3e38367bce475c17c903a40852f61c0465d6985a
ser <140543954+serlovesferrets@users.noreply.github.com>
2024-09-09 17:41:18 +0200

    added maps for expanding / shrinking treesitter selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,13 @@ return {
         config = function()
             require("nvim-treesitter.configs").setup({
                 highlight = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        node_incremental = "v",
+                        node_decremental = "V",
+                    },
+                },
             })
         end,
     },
