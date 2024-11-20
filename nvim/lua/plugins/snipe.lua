589521d2583cf4bcb1d7d5cd3b9591b8c88078ff
ser <140543954+serlovesferrets@users.noreply.github.com>
2024-11-20 10:49:16 +0100

    Added conf for snipe.nvim

--- nvim/lua/plugins/snipe.lua
+++ nvim/lua/plugins/snipe.lua
@@ -1,5 +1,27 @@
 return {
     "leath-dub/snipe.nvim",
+    config = function()
+        local snipe = require("snipe")
+
+        snipe.ui_select_menu = require("snipe.menu"):new()
+
+        snipe.ui_select_menu:add_new_buffer_callback(function(m)
+            vim.keymap.set("n", "<esc>", function()
+                m:close()
+            end, { nowait = true, buffer = m.buf })
+        end)
+
+        vim.ui.select = snipe.ui_select
+
+        snipe.setup({
+            ui = {
+                position = "center",
+            },
+            hints = {
+                dictionary = "arstgmneioblfuwyq",
+            },
+        })
+    end,
     keys = {
         {
             "<leader>t",
@@ -7,12 +29,6 @@ return {
                 require("snipe").open_buffer_menu()
             end,
             desc = "Open Snipe buffer menu",
-        },
-    },
-    opts = {
-        ui = { position = "center" },
-        hints = {
-            dictionary = "arstgmneioblfuwyq",
         },
     },
 }
